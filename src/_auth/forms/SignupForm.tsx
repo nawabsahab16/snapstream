57061fa0361907c744ce72cf4ce94f6ef1820ef8
nawabsahab16 <nawab162004@gmail.com>
2023-11-01 21:51:33 +0530

    feat: shadcn ui components

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/button"
 
 
 const SignupForm = () => {
   return (
-    <div>SignupForm</div>
+    <div>
+      <Button>Click Me</Button>
+    </div>
   )
 }
 
